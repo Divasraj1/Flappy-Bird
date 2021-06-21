2cb8a0999c1cce568c7f203ea1bf4021b371d2fe
DRV <56160725+Divasraj1@users.noreply.github.com>
2021-06-21 19:58:41 +0530

    Updated background
    background movment script added

---
+++ FlappyBirdbyDivasraj/Assets/Scripts/MoveBackground.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBackground : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] float offset;
+    private Vector2 startPos;
+    private float newXpos;
+    BirdController birdController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        birdController = FindObjectOfType<BirdController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (birdController.dead == false)
+        {
+            newXpos = Mathf.Repeat(Time.time * -moveSpeed, offset);
+            transform.position = startPos + Vector2.right * newXpos;
+        }
+    }
+}
